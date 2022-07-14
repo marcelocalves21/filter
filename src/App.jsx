eaae6ac5ac54826d1c0445dae40a4066206d2719
Marcelo C Alves <51497235+marcelocalves21@users.noreply.github.com>
2022-07-14 11:16:54 -0400

    creating group card

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,16 @@
 import './App.css'
 import { Card } from './components/card'
 
+import { pokemons } from './constants/pokemon'
+
 function App() {
 
   return (
     <div className="App">
-      <Card />
+      {pokemons.map((pokemon, index) => {
+        return <Card pokemon={pokemon} />
+      })}
+      
     </div>
   )
 }
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -2,18 +2,17 @@ import Pikachu from '../assets/pikachu.jpeg'
 import Frame from '../assets/frame.png'
 import './card.css'
 
-export const Card = () => {
-
+export const Card = (props) => {
+    console.log(props.type)
     return (
         <div className="card">
             <div className="card-header">
-                <img src={Frame} alt="" srcset="" />
+                <img src={Frame} alt="" srcset="" style={{backgroundImage: `url(${Pikachu})`}} />
             </div>
             <div className="card-body">
                 <span>#025</span>
                 <h3>PIKACHU</h3>
                 <div className="pokemon-type">
-                    <span>Electric</span>
                     <span>Electric</span>
 
                 </div>
